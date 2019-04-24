5e173c3c76a80c141e5d61f0ce75e379ed2c5342
igulevskiy <gulevskiy@i-teco.ru>
2019-04-24 16:21:32 +0300

    Swagger

--- src/java/sb/HelloController.java
+++ src/java/sb/HelloController.java
@@ -1,11 +1,15 @@
 package sb;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "Swagger2DemoRestController", description = "REST APIs related to Student Entity!!!!")
 @RestController
 public class HelloController {
 
+    @ApiOperation(value = "Get list of Students in the System ", response = String.class)
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
